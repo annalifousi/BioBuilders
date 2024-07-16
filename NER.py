3c46bb453c03846f9bde1e6b39c0b5cfdfa7bbec
s232996 <s232996@dtu.dk>
2024-07-16 12:16:47 +0200

    ner files

--- NER.py
+++ NER.py
@@ -3,6 +3,12 @@ import spacy
 from scispacy.abbreviation import AbbreviationDetector
 import en_ner_bionlp13cg_md
 import en_ner_bc5cdr_md
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem.porter import PorterStemmer #add stemming
+import nltk #nltk for the stemming
+
+# Download NLTK data
+nltk.download('punkt')#bag of words
 
 # Load the models
 print("Loading models...")
@@ -16,10 +22,40 @@ nlp_bi.add_pipe("abbreviation_detector")
 nlp_bc.add_pipe("abbreviation_detector")
 print("AbbreviationDetector added successfully.")
 
+
+
+# Read EDCs from androgen_EKDB.tsv file
+print("Reading EDCs from androgen_EKDB.tsv...")
+androgen_df = pd.read_csv('EDC_androgen_catalog.tsv', sep='\t')
+edc_androgen_names = set(androgen_df['Name'].str.lower().unique())  # Get unique EDC names and convert to lowercase
+
+# Read EDCs from estrogen.tsv file
+print("Reading EDCs from estrogen.tsv...")
+estrogen_df = pd.read_csv('EDC_estrogen_catalog.tsv', sep='\t')
+edc_estrogen_names = set(estrogen_df['Name'].str.lower().unique())  # Get unique EDC names and convert to lowercase
+
+# Read EDCs from EDC_catalog_deduct.tsv file
+print("Reading EDCs from EDC_catalog_deduct.tsv...")
+deduct_df = pd.read_csv('EDC_catalog_deduct.tsv', sep='\t')
+edc_deduct_names = set(deduct_df['Name'].str.lower().unique())  # Get unique EDC names and convert to lowercase
+
+print("Reading EDCs from manual_catalog.tsv...")
+manual_df = pd.read_csv('manual_catalog.tsv', sep='\t')
+manual_df_names = set(manual_df['Name'].str.lower())  # Get unique EDC names and convert to lowercase
+
+# Combine all unique EDC names
+all_edc_names = edc_androgen_names.union(edc_estrogen_names).union(edc_deduct_names).union(manual_df_names)
+
+all_edc_names.to_csv('combined_edc_catalog.tsv', index=False)
+
 # Load EDC terms from the combined catalog
 print("Loading EDC terms from combined_edc_catalog.tsv...")
 edc_df = pd.read_csv('combined_edc_catalog.tsv', sep='\t')
 edc_terms = set(edc_df['Name'].str.lower())  # Convert to a set for quick lookup
+
+# Initialize stemmer
+stemmer = PorterStemmer()#bag of words
+
 
 
 # Function to perform additional checks for chemical entities
@@ -71,8 +107,10 @@ def ner(text, pmcid, table, model_name):
  #Filter entities by specific classes
         if ent_label in ["ENDOCRINE_DISRUPTING_CHEMICAL", "SIMPLE_CHEMICAL", "CHEMICAL"]:
             # Add entity to the table
+            stemmed_entity = stemmer.stem(ent.text.lower())
             table["ID"].append(pmcid)
             table["Entity"].append(ent.text)
+            table["StemmedEntity"].append(stemmed_entity) #bag of words
             table["Class"].append(ent_label)
             table["LongForm"].append(abbreviations.get(ent.text.lower(), ""))
             table["FollowedByVerb"].append(is_followed_by_verb)
@@ -87,13 +125,13 @@ print("CSV file read successfully.")
 # Replace NaN values with empty strings
 df = df.fillna("")
 
-df_subset = df.head(10)  # Adjust the subset size as needed
+df_subset = df.head(500)  # Adjust the subset size as needed
 
 # Initialize the table for storing entities
-table = {"ID": [], "Entity": [], "Class": [], "LongForm": [], "FollowedByVerb": []}
+table = {"ID": [], "Entity": [], "StemmedEntity": [], "Class": [], "LongForm": [], "FollowedByVerb": []}
 
 # Iterate through each row in the DataFrame
-for index, row in df.iterrows():
+for index, row in df_subset.iterrows():
     pmcid = row['PMC ID']
     title = row['Title']
     abstract = row['Abstract']
